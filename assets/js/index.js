b8cda87125a3379d9636f2f72c0ec36580976637
EvgenStr <juve2910@gmail.com>
2021-04-27 12:32:15 +0300

    home work init

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,12 +28,35 @@
 // }
 
 const buttons = document.querySelectorAll("button");
-const setColor = ({ target: 
-                    { dataset, parentNode } 
-                  }) => {
+const setColor = ({ target:
+  { dataset, parentNode }
+}) => {
   parentNode.style.backgroundColor = dataset.color;
 };
 
 for (const btn of buttons) {
   btn.addEventListener("click", setColor);
 }
+
+
+const form = document.getElementById("form");
+const arr = [];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const { target, target: { elements: { test: { value } } } } = event;
+  arr.push(value);
+  target.reset();
+  const li = document.createElement('li');
+  li.innerText = value;
+  const del = document.createElement('button');
+  del.innerText = "delete";
+  del.addEventListener('click', (e) => {
+    arr.splice(arr.indexOf(e.target.parentNode.childNodes[0].data), 1);
+    console.dir(e.target.parentNode.childNodes[0].data)
+    e.target.parentNode.remove()
+  })
+  li.append(del);
+  const ul = document.getElementById('list');
+  ul.append(li);
+  })
